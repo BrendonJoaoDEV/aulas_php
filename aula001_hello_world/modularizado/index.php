486b11d4b6841ab130a9c8153bc63170103012b9
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-08-04 21:42:42 -0300

    AULA001: Modularização no hello world - Início

---
+++ aula001_hello_world/modularizado/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+
+    <!-- Título da página -->
+    <h1>Envie sua mensagem</h1>
+
+    <!-- 
+        Formulário para o usuário digitar e enviar dados.
+        - method="POST": indica que os dados serão enviados "por trás" (sem aparecer na URL).
+        - action=processa.php" diz que os dados serão enviados para a página chamada processa.php
+    -->
+</body>
+</html>
