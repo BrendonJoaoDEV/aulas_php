2f27d88dc6982d6ab90aa3b1b8e08c9f8db596b4
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-08-05 21:39:14 -0300

    ATIVIDADE001: Front-end - Início

---
+++ atividades/atividade001_operadores_aritmeticos/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora em PHP</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <header>
+        <h1>Calculadora: Operadores Aritméticos</h1>
+    </header>
+
+    <main>
+        <form id="formulario" method="POST" action="resultado.php">
+            <label for="primeiro-numero">Digite o primeiro número:</label>
+            <input id="primeiro-numero" name="primeiro-numero" type="number" required>
+
+            <label for="segundo-numero">Digite o segundo número:</label>
+            <input id="segundo-numero" name="segundo-numero" type="number" required>
+
+            <label for="escolha-operacao">Escolha a operação:</label>
+            <select id="escolha-operacao" name="escolha-operacao">
+                <option value="selecione">--Selecione--</option>
+                <option value="">Soma (+)</option>
+                <option value="">Subtração (-)</option>
+                <option value="">Multiplicação (&#215;)</option>
+                <option value="">Divisão (&#247;)</option>
+            </select>
+
+            <button id="botao-calcular" name="botao-calcular" type="submit"></button>
+        </form>
+    </main>
+</body>
+</html>
