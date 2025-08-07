727153066c2a996bb9006e4da5b1a500ae8f16cb
Brendon João <brendonjoaodev@gmail.com>
2025-08-07 17:53:49 -0300

    ATIVIDADE001: Front-end - Adição de mais validações

--- atividades/atividade001_operadores_aritmeticos/js/script.js
+++ atividades/atividade001_operadores_aritmeticos/js/script.js
@@ -2,25 +2,38 @@ document.addEventListener('DOMContentLoaded', () => {
     const formulario = document.getElementById('formulario');
     const primeiroNumero = document.getElementById('primeiro-numero');
     const segundoNumero = document.getElementById('segundo-numero');
-    const saidaErro = document.getElementById('erro')
+    const operacao = document.getElementById('escolha-operacao');
+    const saidaErro = document.getElementById('erro');
 
-    formulario.addEventListener('submit', (event) => {
-        let validacao = validarNumero(primeiroNumero);
+    formulario.addEventListener('submit', () => {
+        let validacao = validarCalculo(primeiroNumero.value, segundoNumero.value, operacao.value);
         if (!validacao.valido) {
+            console.log(validacao.erro);
             saidaErro.textContent = validacao.erro;
         }
+    });
+});
 
-        validacao = validarNumero(segundoNumero);
-        if (!validacao.valido) {
-            saidaErro.textContent = validacao.erro;
+function validarCalculo(numero1, numero2, operacao) {
+    if (!numero1 || numero1.trim() === "") {
+        return {valido: false, erro: "Por favor preencha o primeiro campo!"}
+    }
+
+    if (!numero2 || numero2.trim() === "") {
+        return {valido: false, erro: "Por favor preencha o segundo campo!"}
+    }
+
+    if (numero1 == Number(numero1)) {
+        if (numero2 == Number(numero2)) {
+            if (numero2 == 0 && operacao === "divisao") {
+                return {valido: false, erro: "Divisão por zero não permitida!"}
+            } else {
+                return {valido: true, erro: "Tudo certo!"}
+            }
+        } else {
+            return {valido: false, erro: "Por favor preencha o segundo campo com um número!"}
         }
-    })
-})
-
-function validarNumero(numero) {
-    if (numero == Number(numero)) {
-        return {valido: true, erro: "Tudo certo!"}
     } else {
-        return {valido: false, erro: "Por favor digite um número!"}
+        return {valido: false, erro: "Por favor preencha o primeiro campo com um número!"}
     }
 }
