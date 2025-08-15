9d7a4093aa5caf6773d62d54b17fe65822700bb7
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-08-14 21:37:14 -0300

    ATIVIDADE002: G - Temperaturas - Início

---
+++ atividades/atividade002_funcoes_php/g_temperaturas/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversor de temperaturas em PHP</title>
+</head>
+<body>
+    <header>
+        <h1>Conversor de temperaturas em PHP</h1>
+    </header>
+
+    <main>
+        <section>
+            <form method="POST">
+                <input id="etr-temperatura" name="etr-temperatura" placeholder="Digite a temperatura" type="number" required>
+
+                <select id="etr-convercao" name="etr-convercao">
+                    <option value="c => k">°Celsius para Kelvin</option>
+                    <option value="c => f">°Celsius para °Fahrenheit</option>
+                    <option value="f => c">°Fahrenheit para °Celsius</option>
+                    <option value="f => k">°Fahrenheit para Kelvin</option>
+                    <option value="k => c">Kelvin para °Celsius</option>
+                    <option value="k => f">Kelvin para °Fahrenheit</option>
+                </select>
+
+                <button id="btn-converter" type="submit">Converter</button>
+            </form>
+
+            <p id="erro"></p>
+        </section>
+
+        <section>
+            <?php
+                include "public/processa.php";
+            ?>
+        </section>
+    </main>
+    
+    <footer>
+        <p>&copy; 2025 - Brendon J. C. Neves</p>
+    </footer>
+</body>
+</html>
---
+++ atividades/atividade002_funcoes_php/g_temperaturas/public/processa.php
@@ -0,0 +1,19 @@
+<?php
+    function celsiusKelvin ($temperatura_inicial) {
+
+    }
+
+    function celsiusFahrenheit ($temperatura_inicial) {
+
+    }
+
+    function fahrenheitCelsius ($temperatura_inicial) {
+
+    }
+
+    function fahrenheitKelvin ($temperatura_inicial) {
+
+    }
+
+    
+?>
