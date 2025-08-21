78a1fa0732d8a647d7b354aa9256996db76c481a
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-08-21 20:22:23 -0300

    ATIVIDADE004: G - Temperaturas - Funcionamento básico

--- atividades/atividade002_funcoes_php/g_temperaturas/public/processa.php
+++ atividades/atividade002_funcoes_php/g_temperaturas/public/processa.php
@@ -1,31 +1,43 @@
 <?php
     function celsiusKelvin ($temperatura_inicial) {
-
+        $calculo = $temperatura_inicial + 273;
+        $resultado = "<strong>$temperatura_inicial °Celsius</strong> equivale a <strong>$calculo Kelvin</strong>!";
+        return $resultado;
     }
 
     function celsiusFahrenheit ($temperatura_inicial) {
-
+        $calculo = (($temperatura_inicial * 9) / 5) + 32;
+        $resultado = "<strong>$temperatura_inicial °Celsius</strong> equivale a <strong>$calculo °Fahrenheit</strong>!";
+        return $resultado;
     }
 
     function fahrenheitCelsius ($temperatura_inicial) {
-
+        $calculo = (($temperatura_inicial - 32) *5) / 9;
+        $resultado = "<strong>$temperatura_inicial °Fahrenheit</strong> equivale a <strong>$calculo °Celsius</strong>!";
+        return $resultado;
     }
 
     function fahrenheitKelvin ($temperatura_inicial) {
-
+        $calculo = ((($temperatura_inicial - 32) *5) / 9) + 273;
+        $resultado = "<strong>$temperatura_inicial °Fahrenheit</strong> equivale a <strong>$calculo Kelvin</strong>!";
+        return $resultado;
     }
 
     function kelvinCelsius ($temperatura_inicial) {
-
+        $calculo = $temperatura_inicial -273;
+        $resultado = "<strong>$temperatura_inicial Kelvin</strong> equivale a <strong>$calculo °Celsius</strong>!";
+        return $resultado;
     }
 
     function kelvinFahrenheit ($temperatura_inicial) {
-
+        $calculo = ((($temperatura_inicial - 273)) * (9/5)) + 32;
+        $resultado = "<strong>$temperatura_inicial Kelvin</strong> equivale a <strong>$calculo °Fahrenheit</strong>!";
+        return $resultado;
     }
 
     if (isset($_POST['etr-temperatura'])) {
         $temperatura_inicial = $_POST['etr-temperatura'];
-        switch $_POST['etr-convercao'] {
+        switch ($_POST['etr-convercao']) {
             case "c => k":
                 $resultado = celsiusKelvin($temperatura_inicial);
                 break;
@@ -44,8 +56,8 @@
             case "k => f":
                 $resultado = kelvinFahrenheit($temperatura_inicial);
                 break;
-            defalut:
-                $resultado = "<strong>Nenhuma opção selecionada!</strong>"
+            default:
+                $resultado = "<strong>Nenhuma opção selecionada!</strong>";
                 break;
         }
     }
