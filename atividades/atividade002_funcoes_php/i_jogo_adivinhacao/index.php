3bffea5512e40509e214107b8983a15d48637c54
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-08-21 21:36:21 -0300

    ATIVIDADE004: I - Jogo de adivinhação - Início

---
+++ atividades/atividade002_funcoes_php/i_jogo_adivinhacao/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jogo de adivinhação em PHP</title>
+</head>
+<body>
+    <header>
+        <h1>Jogo de adivinhação em PHP</h1>
+    </header>
+
+    <main>
+        <section>
+            <form method="POST">
+                <label for="etr-chute">Valor do palpite</label>
+                <input id="etr-chute" name="etr-chute" type="number" required>
+
+                <button id="btn-adivinhar">Tentar adivinhar</button>
+            </form>
+        </section>
+
+        <section>
+            <?php
+                if (isset())
+            ?>
+        </section>
+    </main>
+
+    <footer>
+        <p>&copy; 2025 - Brendon J. C. Neves</p>
+    </footer>
+</body>
+</html>
